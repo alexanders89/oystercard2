1553e55d9822b01ec3b97077ed8a58f3a2e77f74
Alex <alex.satur89@gmail.com>
2017-05-19 10:15:37 +0100

    Fixed errors in step 14 regarding no touch in or touch out

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,27 +1,30 @@
 class Journey
 
   PENALTY_FARE = 6
+  MINIMUM_FARE = 2
 
   def initialize
-    @journey = []
+    @journey = {start: nil, finish: nil, fare: nil}
   end
 
   def complete?
   end
 
   def fare
-    PENALTY_FARE
+    @journey[:fare] = PENALTY_FARE if @journey[:start].nil? || @journey[:finish].nil?
+    @journey[:fare] = MINIMUM_FARE if !@journey[:start].nil? && !@journey[:finish].nil?
   end
 
   def start(entry_station)
-    fail "You have already touched in" if @journey.count > 1
-    @journey << entry_station
+    #fail "You have already touched in" if !@journey[:start].nil?
+    @journey[:start] = entry_station
     self
   end
 
   def finish(exit_station)
-    fail "You have already touched out" if @journey.count > 1
-    @journey << exit_station
+    #fail "You have already touched out" if !@journey[:finish].nil?
+    @journey[:finish] = exit_station
+    fare
     self
   end
 
