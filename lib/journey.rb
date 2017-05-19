d203789ce11412677218ffc90d42068967e8ddf3
Alex <alex.satur89@gmail.com>
2017-05-19 16:26:32 +0100

    More testing

--- lib/journey.rb
+++ lib/journey.rb
@@ -8,17 +8,30 @@ class Journey
     @info = { start: nil, finish: nil, fare: nil }
   end
 
-  def complete?
+  def incomplete?
+    return true if @info[:start].nil? || @info[:finish].nil?
+    false
+  end
+
+  def terminate_incomplete_journey
+    if incomplete?
+      fare
+      return @info
+    end
   end
 
   def fare
     @info[:fare] = PENALTY_FARE if @info[:start].nil? || @info[:finish].nil?
     @info[:fare] = MINIMUM_FARE if !@info[:start].nil? && !@info[:finish].nil?
+    nil
   end
 
   def start(entry_station)
     # fail "You have already touched in" if !@info[:start].nil?
+    terminate_incomplete_journey
+
     @info[:start] = entry_station
+    fare
     self
   end
 
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,7 +2,7 @@ require_relative 'journey'
 
 class Oystercard
 
-  attr_reader :balance, :entry_station, :exit_station, :journeys
+  attr_reader :balance, :entry_station, :exit_station, :journeys, :journey
 
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1
@@ -10,10 +10,14 @@ class Oystercard
   def initialize(balance = 0)
     @balance = balance
     @journeys = []
+    @journey_started = false
+    @journey = Journey.new
   end
 
-  def in_journey
-    @journey ||= Journey.new
+  def in_journey?
+    return true if @journey.info[:start] != nil && @journey.info[:finish] == nil
+    return true if @journey.info[:start] == nil && @journey.info[:finish] != nil
+    false
   end
 
   def top_up(amount)
@@ -23,26 +27,37 @@ class Oystercard
 
   def touch_in(entry_station)
     raise 'Balance too low : Top up Please' if @balance < MINIMUM_BALANCE
-    in_journey
+    if in_journey?
+      save_journey
+      deduct(journey.info[:fare])
+    end
+    @journey = journey
     @journey.start(entry_station)
+    @journey_started = true
   end
 
   def touch_out(exit_station)
-    in_journey
+    @journey = Journey.new if !@journey_started
     @journey.finish(exit_station)
-    @journeys << @journey
-    @journey = nil
+    deduct(journey.info[:fare])
+    @journey_started = false
+    save_journey
   end
 
   private
 
+  def save_journey
+    @journeys << @journey.info
+  end
+
   def deduct(amount)
     @balance -= amount
   end
+
 end
 
-# p card = Oystercard.new
-# p card.top_up(10)
-# p card.touch_in(:A)
-# p card.touch_out(:B)
-# p card
+p card = Oystercard.new
+p card.top_up(10)
+p card.touch_in(:A)
+p card.touch_out(:B)
+p card
